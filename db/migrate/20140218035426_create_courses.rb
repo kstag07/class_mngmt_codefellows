27becafdba8591a4c197b0e17eab317d920c7b0c
Kevin Staiger <kstag07@yahoo.com>
2014-02-17 20:28:26 -0800

    creating one to many relationship

---
+++ db/migrate/20140218035426_create_courses.rb
@@ -0,0 +1,15 @@
+class CreateCourses < ActiveRecord::Migration
+  def change
+    create_table :courses do |t|
+      t.integer :student_id
+      t.string :title
+      t.text :description
+      t.date :start_date
+      t.date :end_date
+      t.string :location
+      t.string :semester
+
+      t.timestamps
+    end
+  end
+end
