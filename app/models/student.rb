cfaa09ef07927744500f963bb41aef6f981cf14f
Kevin Staiger <kstag07@yahoo.com>
2014-02-19 19:27:24 -0800

    Saved scopes on students model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,12 @@
 class Student < ActiveRecord::Base
     
-    has_many :courses
+    #has_many :courses
+    has_many :registrations 
+    has_many :courses, through: :registrations
+    
+    def self.teachers
+        where(title: 'Teacher')
+        end 
     
     def combined_info
         "name: #{full_name} \n" +
